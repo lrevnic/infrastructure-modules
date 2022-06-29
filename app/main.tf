07b5b146a66f92b2c7b5f2fa9a768495806f5c49
revnic <teodor-lucian.revnic@microfocus.com>
2022-06-29 16:59:16 +0300

    same path

--- app/main.tf
+++ app/main.tf
@@ -7,7 +7,7 @@ terraform {
   backend "s3" {}
 }
 module "app" {
-  source = "app"
+  source = "."
   instance_type  = var.instance_type
   instance_count = var.instance_count
   # ... other app settings ...
